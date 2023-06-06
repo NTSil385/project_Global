bfaeb4d92d6326dfef37a2a2248aef7d4cf726b7
VoThiPhuonNgocTram <tramv4444@gmail.com>
2023-06-06 20:51:33 +0700

    Initial commit

---
+++ src/main/java/com/example/project_Global/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.project_Global.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping ("/")
+    public String showhome() {
+        return "view/index";
+    }
+
+
+
+}
